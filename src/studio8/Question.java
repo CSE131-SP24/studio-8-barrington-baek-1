97b2da46e6b6543efd60cc4cbe8f287e6a9ed20a
mbarr <mbarr@DESKTOP-UFJCE8A.dhcp.wustl.edu>
2024-04-03 14:35:43 -0500

    s8

--- src/studio8/Question.java
+++ src/studio8/Question.java
@@ -1,20 +1,24 @@
 package studio8;
 
+//instance variables
 public class Question {
 	private String prompt;
 	private String answer;
 	private int points;
 	
+	// constructor: set instance variables to parameters
 	public Question(String prompt, String answer, int points) {
 		this.prompt = prompt;
 		this.answer = answer;
 		this.points = points;
 	}
 	
+	//display question
 	public void displayPrompt() {
 		System.out.println(this.prompt + "(" + this.points + " points)");
 	}
 	
+	//check answer 
 	public int checkAnswer(String givenAnswer) {
 		if(answer.equals(givenAnswer)) { //String comparison
 			return this.points;
@@ -23,6 +27,7 @@ public class Question {
 		}
 	}
 	
+	//getters
 	public int getPoints() {
 		return this.points;
 	}
@@ -31,6 +36,7 @@ public class Question {
 		return this.answer;
 	}
 	
+	//main
 	public static void main(String[] args) {
 		Question q = new Question("What number studio is this?", "8", 2);
 		q.displayPrompt();
